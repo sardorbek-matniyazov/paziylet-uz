f2730dcee0f9f7373a118905faa1d6b6547b816c
sardorbek-matniyazov <sardorbekmatniyazov03@gmail.com>
2022-12-13 23:18:40 +0500

    imports bug here

--- src/main/java/paziylet_uz/controller/PostController.java
+++ src/main/java/paziylet_uz/controller/PostController.java
@@ -2,7 +2,12 @@ package paziylet_uz.controller;
 
 import com.google.gson.Gson;
 import org.springframework.http.HttpEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import paziylet_uz.payload.dao.MyResponse;
 import paziylet_uz.payload.dto.PostDto;
